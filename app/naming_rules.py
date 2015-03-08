3f985d5295757623c77c04f4a49f00b24163fe7f
Stephen Gaffney <stephen.gaffney@yale.edu>
2015-03-08 11:51:13 -0400

    Add naming rules script.

---
+++ app/naming_rules.py
@@ -0,0 +1,10 @@
+
+
+
+def get_dir_path(upload_obj):
+    return
+
+def get_tree_score_paths(upload_obj):
+    """Get path tuple () from upload object."""
+    proj_suffix = user_upload.get_local_filename()
+    os.path.join(dir_path, proj_suffix + '_scores.txt')
