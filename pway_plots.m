99eaefb7b05bb525b6538b876ec7f5a8b1edf0d7
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-06-06 14:38:34 +0100

    Remove Matlab files.

--- pway_plots.m
+++
@@ -1,8 +0,0 @@
-function pway_plots(results_file, scores_file, names_file, svg_out_path, ...
-    genome_size, hypermutated_path)
-
-plot_pway_dendrogram(scores_file, names_file, svg_out_path);
-plot_pway_targets(results_file, genome_size, '--svg','--skipfew');
-plot_patient_genes(results_file, '--hypermutated', hypermutated_path);
-
-end
