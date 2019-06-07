dae3c24514ecd52ff4e3975006334d256e7d5ecc
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-06-07 10:19:20 +0100

    Simplify celery_worker using dotenv.

--- celery_worker.py
+++ celery_worker.py
@@ -1,10 +1,5 @@
-import os
-if os.path.exists('.env'):
-    print('Importing environment from .env...')
-    for line in open('.env'):
-        var = line.strip().split('=')
-        if len(var) == 2:
-            os.environ[var[0]] = var[1]
+from dotenv import load_dotenv
+load_dotenv()
 
 
 from app import celery, create_app
