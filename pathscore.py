3ccce12ca6675be71027e860165abc5b0eddc3a7
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-08-15 17:50:09 +0100

    Add pathscore.py module for alt serving with flask.

---
+++ pathscore.py
@@ -0,0 +1,15 @@
+import os
+import logging
+from dotenv import load_dotenv, find_dotenv
+
+
+logger = logging.getLogger(__name__)
+
+# LOAD .env file from location specified by ENV_NAME environment variable
+env_path = os.getenv('ENV_NAME', find_dotenv())
+logger.info("Loading .env from %s", env_path)
+load_dotenv(env_path, override=True)
+
+from app import create_app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
