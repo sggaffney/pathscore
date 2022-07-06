d4935ac463645487b0bd15f53658bcd28b72ab78
Stephen Gaffney <stephen.gaffney@yale.edu>
2022-07-06 12:17:18 +0100

    Tidy compare_projects.py, improving docstrings.

--- helpers/compare_projects.py
+++ helpers/compare_projects.py
@@ -1,9 +1,11 @@
 import re
 import logging
 from collections import Counter
+from typing import Iterable
 
 import pandas as pd
 import scipy.stats as stats
+from pandas.io.formats.style import Styler
 
 
 _logger = logging.getLogger(__name__)
@@ -14,28 +16,16 @@ STRIP_PREFIXES = {'KEGG_', 'REACTOME_', 'PID_', 'BIOCARTA_'}
 GREENS = ['#e5ffe5', '#e5ffe5', '#e3fee3', '#e1fde1', '#e0fce0', '#defbde', '#dcfadc', '#dbf9db', '#daf9da', '#d8f8d8', '#d6f7d6', '#d5f6d5', '#d3f5d3', '#d2f4d2', '#d0f3d0', '#cff3cf', '#cdf2cd', '#cbf1cb', '#caf0ca', '#c9efc9', '#c7eec7', '#c5edc5', '#c4edc4', '#c2ecc2', '#c1ebc1', '#c0eac0', '#bee9be', '#bce8bc', '#bae7ba', '#b9e7b9', '#b8e6b8', '#b6e5b6', '#b5e4b5', '#b3e3b3', '#b1e2b1', '#b0e1b0', '#afe1af', '#ade0ad', '#abdfab', '#a9dea9', '#a8dda8', '#a6dca6', '#a5dba5', '#a4dba4', '#a2daa2', '#a0d9a0', '#9ed89e', '#9ed79e', '#9cd69c', '#9ad59a', '#99d599', '#97d497', '#95d395', '#94d294', '#93d193', '#91d091', '#8fcf8f', '#8ecf8e', '#8cce8c', '#8bcd8b', '#89cc89', '#88cb88', '#86ca86', '#84c984', '#83c983', '#82c882', '#80c780', '#7ec67e', '#7dc57d', '#7bc47b', '#7ac37a', '#79c379', '#77c277', '#75c175', '#73c073', '#72bf72', '#70be70', '#6fbd6f', '#6dbc6d', '#6cbc6c', '#6abb6a', '#68ba68', '#67b967', '#66b866', '#64b764', '#62b662', '#61b661', '#5fb55f', '#5eb45e', '#5db35d', '#5bb25b', '#59b159', '#57b057', '#56b056', '#55af55', '#53ae53', '#52ad52', '#50ac50', '#4eab4e', '#4daa4d', '#4caa4c', '#4aa94a', '#48a848', '#47a747', '#45a645', '#44a544', '#42a442', '#41a441', '#3fa33f', '#3da23d', '#3ca13c', '#3aa03a', '#399f39', '#379e37', '#369e36', '#349d34', '#329c32', '#319b31', '#309a30', '#2e992e', '#2c982c', '#2b982b', '#299729', '#289628', '#269526', '#259425', '#239323', '#219221', '#209220', '#1f911f', '#1d901d', '#1b8f1b', '#1a8e1a', '#188d18', '#178c17', '#168c16', '#148b14', '#128a12', '#108910', '#0f880f', '#0e870e', '#0c860c', '#0b860b', '#098509', '#078407', '#058305', '#058205', '#038103', '#018001', '#008000']
 
 
-def parse_cov_struct(cov_struct):
-    """Convert PathScore coverage string to coverage dictionary.
-
-    Example input: "struct('PTPN13',20.00,'RGS3',20.00,'SRC',20.00)"
-    """
-    try:
-        vals = cov_struct[7:][:-1].replace("'", "").split(',')
-    except TypeError:
-        return {}
-    genes = vals[0::2]
-    covs = [float(i) for i in vals[1::2]]
-    return dict(zip(genes, covs))
-
-
-def gather_pathways(categ_names, tsv_dict, n_dict):
-    """Build table for PathScore project comparison.
+def gather_pathways(categ_names: Iterable, tsv_dict: dict, n_dict: dict) -> pd.DataFrame:
+    """Build pathway summary stats table for multiple projects.
 
     Args:
-        categ_names (iterable): Project names
-        tsv_dict (dict): maps Project name -> tsv path for all_pways file.
-        n_dict (dict): Project name -> number of cases in project. Note: should
+        categ_names: Ordered iterable of descriptive names for project.
+        tsv_dict: maps categ_name -> path for all_pways tsv file.
+        n_dict: Project name -> number of cases in project. Note: should
             also include cases without any variants.
+    Returns:
+        DataFrame: 1 row per pathway, columns are per-project statistics.
     """
     df_dict = {}
     for categ in categ_names:
@@ -58,7 +48,7 @@ def gather_pathways(categ_names, tsv_dict, n_dict):
     df[('n_genes', '')] = df['n_genes'].apply(
         lambda v: v.dropna().iloc[0], axis=1).astype(int)
     df.drop(columns=[('n_genes', v) for v in categ_names], inplace=True)
-    df['gene_coverage'] = df['gene_coverage'].applymap(parse_cov_struct)
+    df['gene_coverage'] = df['gene_coverage'].applymap(_parse_cov_struct)
     # Add condensed gene list
     genes = df['gene_coverage'].applymap(lambda v: _get_genes_display(v))
     genes.columns = pd.MultiIndex.from_tuples([('genes', i) for i in genes.columns])
@@ -80,31 +70,33 @@ def gather_pathways(categ_names, tsv_dict, n_dict):
         df[('effect_floor', categ)] = temp[categ].where(temp[categ].fillna(0) > 1, 1)
     # Get shortened pathway names without database
     short_names = get_short_names(df.index.levels[1].values)
-    df.index.set_levels(short_names, level='pathway_name', inplace=True)
+    new_index = df.index.set_levels(short_names, level='pathway_name')
+    df.index = new_index
     return df
 
 
-def identify_enrichment(ref_name, compared_name, df, other_compared=None,
-                        excel_out=False,
-                        ):
-    """Compare enrichment for project pair.
+def identify_enrichment(ref_name: str, compared_name: str, df: pd.DataFrame,
+                        other_compared: Iterable = None, excel_out: bool = False,
+                        ) -> pd.DataFrame:
+    """Build enrichment comparison table for enrichment relative to a reference project.
 
     Args:
-        ref_name (str): Project name for enriched pathways
-        compared_name (str): Project name for comparison
-        df (pd.DataFrame): pathway stats table, output from gather_pathways.
-        other_compared (iterable): other project names for calculating
-            proportion P value.
+        ref_name: Reference project name. Enrichment is assessed relative to
+            this project.
+        compared_name: Name of project to be assessed relative to reference project.
+        df: pathway stats table, output from gather_pathways.
+        other_compared (iterable): (optional) names of other projects to include
+            in table, adding columns with proportion test P values vs reference.
         excel_out (bool): if True, write comparisons to Excel files.
     Returns:
-        dictionary of project name -> enrichment dataframe
+        Dataframe with enrichment statistics.
     """
 
     show_cols = ['pc_n', 'p_fisher', 'n_genes_mutated', 'n_genes', 'mut_genes',
                  'p_value', 'effect_floor', 'rank', ]  # 'pc_cov', 'n_cov', n_miss
 
-    all_names = [i for i in df.columns.levels[1] if i]
-    names_rest = [i for i in all_names if i != ref_name]
+    # all_names = [i for i in df.columns.levels[1] if i]
+    # names_rest = [i for i in all_names if i != ref_name]
     other_compared = [] if other_compared is None else other_compared
 
     _logger.info(f"Comparing enrichment in {ref_name} to {compared_name}.")
@@ -124,7 +116,6 @@ def identify_enrichment(ref_name, compared_name, df, other_compared=None,
         # temp[('p_fisher', 'min')] = temp['p_fisher'].min(axis=1)
     # temp.sort_values('effect_floor_diff', ascending=False, inplace=True)
     temp['low_p'] = temp['p_value'][ref_name] < 0.05
-    # temp[low_p]
 
     temp.sort_values(('rank', ref_name), ascending=True, inplace=True)
     out = temp.loc[(temp['low_p'] & (temp['effect_floor_diff'] > 0))][show_cols]
@@ -132,16 +123,19 @@ def identify_enrichment(ref_name, compared_name, df, other_compared=None,
     out['pathway_name'] = out['pathway_name'].transform(
         lambda v: v.replace('_', ' '))
     if excel_out:
-        out.to_excel(f'enrichment_{ref_name}_gt_{compared_name}.xlsx')
+        xlsx_path = f'enrichment_{ref_name}_gt_{compared_name}.xlsx'
+        out.to_excel(xlsx_path)
+        _logger.info("Enrichment comparison table written to %s", xlsx_path)
+
     return out.copy()
 
 
-def get_styled_comparison(comp, caption):
+def get_styled_comparison(comp: pd.DataFrame, caption: str) -> Styler:
     """Style enrichment comparison table.
 
     Args:
-        comp (pd.DataFrame): comparison table via identify_enrichment.
-        caption (str): caption text for output.
+        comp: comparison table generated via identify_enrichment.
+        caption: caption text for output.
 
     Returns:
          Styler object.
@@ -169,6 +163,20 @@ def get_styled_comparison(comp, caption):
     # styler.bar(subset=['effect_floor'], color='#5fba7d', vmin=1, vmax=10)
     styler.set_caption(caption)
     return styler
+
+
+def _parse_cov_struct(cov_struct):
+    """Convert PathScore coverage string to coverage dictionary.
+
+    Example input: "struct('PTPN13',20.00,'RGS3',20.00,'SRC',20.00)"
+    """
+    try:
+        vals = cov_struct[7:][:-1].replace("'", "").split(',')
+    except TypeError:
+        return {}
+    genes = vals[0::2]
+    covs = [float(i) for i in vals[1::2]]
+    return dict(zip(genes, covs))
 
 
 def _style_coverage(pcn_val):
